f8c2d8b330c4a5798346997940b59f9937eb8f35
Qibin <qbye0405@gmail.com>
2023-02-03 22:12:53 -0500

    #10

--- 13.js
+++ 13.js
@@ -5,140 +5,150 @@ why it is.
 
 //Problem 1
 function findFirstIndexOfNumber(number, array) {
-    for (let i = 0; i < array.length; i++) { // O(n)
-    if (array[i] === number) { // O(1)
-    return i; 
+  for (let i = 0; i < array.length; i++) {
+    // O(n)
+    if (array[i] === number) {
+      // O(1)
+      return i;
     }
-    }
-    return -1 
+  }
+  return -1;
 }
 //time complexity is linear time because a for...loop is linear and conditional operation is constant, the conslidated time is linear, O(n).
-//space complexity is constant because the final value returns one index value or -1. 
-
+//space complexity is constant because the final value returns one index value or -1.
 
 //problem 2
-    function findEachIndexOfNumber(number,array) { let
-        arrayOfIndexes = [];
-        array.forEach(function(element, index) { // O(n)
-        if (element === number) {
-        arrayOfIndexes.push(index); // O(1)
-        }
-        });
-        return arrayOfIndexes;
-        }
+function findEachIndexOfNumber(number, array) {
+  let arrayOfIndexes = [];
+  array.forEach(function (element, index) { // O(n)
+    if (element === number) {
+      arrayOfIndexes.push(index); // O(1)
+    }
+  });
+  return arrayOfIndexes;
+}
 //The time complexity is linear because forEach method which loops through each element of the array takes linear time to finish the operation while the push method takes constant time to add an element to the end of array. The conslidated time of complexity is linear, O(n).
 //The space complexity is linear because the final value returns in an array.
 
-
 //problem 3
 const array = [36, 14, 1, 7, 21];
-function higherOrLower(array){
-    if (array[array.length -1 ] > array[0]) { 
-        return "Higher";
-    }else if (array[array.length -1 ] < array[0]) {
-        return "Lower";
-    }else{
-        return "Neither";
-    }
+function higherOrLower(array) {
+  if (array[array.length - 1] > array[0]) {
+    return "Higher";
+  } else if (array[array.length - 1] < array[0]) {
+    return "Lower";
+  } else {
+    return "Neither";
+  }
 }
-//The time complexity is constant because the conditional operation takes constant time. 
-//The space complexity is constnat because it returns a fixed size string as a final value. 
-
+//The time complexity is constant because the conditional operation takes constant time.
+//The space complexity is constnat because it returns a fixed size string as a final value.
 
 //problem 4
-const array = [1,2,3,4,5,6,7,8];
-function determineSumOfSequentialArray(array) { 
-    let sum = 0;
-    for (let i = 0; i < array.length; i++) { 
-        sum += array[i];
-    }
-    return sum;
+const array = [1, 2, 3, 4, 5, 6, 7, 8];
+function determineSumOfSequentialArray(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) { // O(n)
+    sum += array[i];
+  }
+  return sum;
 }
-//It takes linear time to complete the computation because the for loop takes linear while the addition takes constant time. 
-//It will take constant space because the final value returns one numeric value. 
-
+//It takes linear time to complete the computation because the for loop takes linear while the addition takes constant time.
+//It will take constant space because the final value returns one numeric value.
 
 //problem 5
-const array = [1,2,3,4,5,6,7,8];
+const array = [1, 2, 3, 4, 5, 6, 7, 8];
 function determineSumOfSequentialArray(array) {
-return array.length * (array.length + 1)/2;
+  return (array.length * (array.length + 1)) / 2;
 }
-//The time complexity is constant because the mathmatic operations takes constant time. 
-//the space complexity is constant because the final value returns a numeric value. 
-
+//The time complexity is constant because the mathmatic operations takes constant time.
+//the space complexity is constant because the final value returns a numeric value.
 
 //problem 6
-function searchSortedArray(number, array, beginIndex = 0, endIndex = array.length - 1) {
-    let middleIndex = Math.floor((beginIndex + endIndex)/2);
-    if (array[middleIndex] === number) {
-        return middleIndex;
-    } else if (beginIndex >= endIndex) {
-        return -1;
-    } else if (array[middleIndex] < number) {
-        beginIndex = middleIndex + 1;
-        return recursiveBinarySearch(number, array, beginIndex, endIndex);
-    } else if (array[middleIndex] > number) {
-        endIndex = middleIndex - 1;
-        return recursiveBinarySearch(number, array, beginIndex, endIndex);
-    }
+function searchSortedArray(
+  number,
+  array,
+  beginIndex = 0,
+  endIndex = array.length - 1
+) {
+  let middleIndex = Math.floor((beginIndex + endIndex) / 2);
+  if (array[middleIndex] === number) { // O(log n)
+    return middleIndex;
+  } else if (beginIndex >= endIndex) {
+    return -1;
+  } else if (array[middleIndex] < number) {
+    beginIndex = middleIndex + 1;
+    return recursiveBinarySearch(number, array, beginIndex, endIndex);
+  } else if (array[middleIndex] > number) {
+    endIndex = middleIndex - 1;
+    return recursiveBinarySearch(number, array, beginIndex, endIndex);
+  }
 }
-//The time compelxity is constant because it's assignment and conditional statement.
-//The space complexity is linear becuase it's a recursive operation. 
-
+//The time compelxity is logrithmic time O(log n) because
+//The space complexity is ??? becuase it's a recursive operation.
 
 //problem 7
 const array1 = [3, 7, 9, 12, 15, 18, 32];
-const array2 = [3, 3, 7, 41, 76]; 
+const array2 = [3, 3, 7, 41, 76];
 function compareArrays(array1, array2) {
-    let arrayOfPairs = [];
-    array1.forEach(function(e, i) {
-        array2.forEach(function(e2, i2) {
-            if (e === e2) {
-            arrayOfPairs.push([i, i2]);
-            }
-        });
+  let arrayOfPairs = [];
+  array1.forEach(function (e, i) {//O(n)
+    array2.forEach(function (e2, i2) {// O(n)
+      if (e === e2) {
+        arrayOfPairs.push([i, i2]); // O(1)
+      }
     });
-    return arrayOfPairs;
+  });
+  return arrayOfPairs;
 }
-//
+//The time complexity is quadratic time, O(n^2) because there's a nested loop.
+//The space complexity is linear because it returns an array.
 
 //problem 8
-function sortByValue(array){
-    function swap(array, index1, index2){ let
-    temporaryValue = array[index1];
-    array[index1] = array[index2];
-    array[index2] = temporaryValue;
-    }
-    let count = 1;
-    while (count < array.length) { let
-    swapCount = 0;
-    for (let i=0; i<array.length-count; i++) {
-    if (array[i] > array[i+1]) {
-    swap(array, i, i+1);
-    swapCount++;
-    }
+function sortByValue(array) {
+  function swap(array, index1, index2) {
+    let temporaryValue = array[index1]; // O(1)
+    array[index1] = array[index2]; // O(1)
+    array[index2] = temporaryValue; // O(1)
+  }
+  let count = 1;
+  while (count < array.length) { // O(n)
+    let swapCount = 0;
+    for (let i = 0; i < array.length - count; i++) { // O(n)
+      if (array[i] > array[i + 1]) {
+        swap(array, i, i + 1); // O(1)
+        swapCount++; // O(1)
+      }
     }
     count++;
-    }
-    return array;
-    }
+  }
+  return array;
+}
+// The time complexity is quadratic time, O(n^2). The swap function takes constant time while the for loop is nested in while loop takes quadratic time. 
+//The space complexity is linear because the final value returns an array.
+
 
 //problem 9
-function returnDupes(array, array2) { let
-    dupeArray = [];
-    array.forEach(function(element) {
-    if (array2.includes(element)) {
-    dupeArray.push(element);
+function returnDupes(array, array2) {
+  let dupeArray = [];
+  array.forEach(function (element) { //O(n)
+    if (array2.includes(element)) { //O(n)
+      dupeArray.push(element); //O(1)
     }
-    });
-    return dupeArray;
-    }
+  });
+  return dupeArray; 
+}
+//The time complexity is O(n^2) because the includes method takes O(n) and since it's inside of forEach method which also takes O(n), the consolidated time is quadratic.
+// The space complexity is linear bacause it returns an array. 
+
 
 //problem 10
 function sumFilteredData(array) {
-    return array.filter(function(element) {
-    return ((element > 5) && (element < 20))
-    }).reduce(function(valueToAdd, currentValue) {
-    return valueToAdd + currentValue;
+  return array.filter(function (element) { 
+      return element > 5 && element < 20;
+    }).reduce(function (valueToAdd, currentValue) {
+      return valueToAdd + currentValue;
     }, 0);
-    }
+}
+// The time complexity is O(n) because it uses the chaining of methods. 
+// The space complexity is linear because it returns a new array. 
