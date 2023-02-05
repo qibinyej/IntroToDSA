d3589700ebee40f3757797c224c2dd5d9beffedc
Qibin <qbye0405@gmail.com>
2023-02-05 00:59:58 -0500

    copied problems

---
+++ day-2/2.js
@@ -0,0 +1,10 @@
+/*
+Write a function called, areThereDuplicates which accepts a array of letters,
+and checks whether there are any duplicates among the argument passed in.
+You can solve this using the frequency counter pattern OR the multiple pointers pattern.
+*/
+const areThereDuplicates = (arrOfLetters) => {
+}
+//Tests
+// areThereDuplicates(['a', 'a', 'c', 'd']) true
+// areThereDuplicates(['a', 'b', 'c']) // false
---
+++ day-2/3.js
@@ -0,0 +1,14 @@
+/*
+Write a function called avgPair.
+Given a sorted array of integers and a target average, determine if there is a pair of values in the
+array
+where the average of the pair equals the target average.
+There may be more than one pair that matches the average target.
+*/
+const avgPair = (arr, avgTarget) => {
+}
+// Test Cases:
+// avgPair([1,2,3],2.5) true
+// avgPair([1,3,3,5,6,7,10,12,19],8) true
+// avgPair([-1,0,3,4,5,6], 4.1) false
+// avgPair([],4) false
