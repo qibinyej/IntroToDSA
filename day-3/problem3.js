d80ba3d5ca65f000676704230d8a33e9260d6766
Qibin <qbye0405@gmail.com>
2023-02-07 16:06:44 -0500

    problem3

--- day-3/problem3.js
+++ day-3/problem3.js
@@ -6,9 +6,16 @@ function lengthOfLongestSubstring(s) {
         
         letterArray.push(s[i])
         //console.log(letterArray)
+
     }
 }
 // console.log(lengthOfLongestSubstring("abcabcbb")); // Output: 3
-//[a, b, c, a, b,c, b, b]
+//[a, b, c, a, b,c, b, b] 
+//freqCounter = {
+    "a": 1+1
+    "b": 1+1+1+1
+    "c": 1+1
+
+}
 // console.log(lengthOfLongestSubstring("bbbbbb")); // Output: 1
 // console.log(lengthOfLongestSubstring("pwwkew")); // Output: 3
