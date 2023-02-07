1deb4168fda5b60efd23c60fd81d4c56f0248326
Qibin <qbye0405@gmail.com>
2023-02-07 16:06:21 -0500

    problem3

--- day-3/problem2.js
+++ day-3/problem2.js
@@ -1,8 +1,28 @@
 // Given a sorted array of integers, find the first occurrence of a target value.
 // If the target is not found in the array, return -1.
 function binarySearchFirstOccurrence(nums, target) {
+    //edge case
+    if(nums.length <=0){
+        return null
+    }
+    //two index pointers: left and right
+    let left = 0; 
+    let right = nums.length-1;
+    //while loop, find middle index
+    while(left<right){
+        let middleIndex = Math.floor((left+right)/2)
+        let middleValue = nums[middleIndex]
+        if(middleValue == target){
+            return middleValue
+        }else if(middleValue < target){
+            left = middleIndex +1
+        }else{
+            right = middleIndex -1
+        }
+    }
 
-    
+    return -1
+
 }
 
 // Test cases
@@ -11,11 +31,13 @@ function binarySearchFirstOccurrence(nums, target) {
 // let expected1 = 2;
 // let result1 = binarySearchFirstOccurrence(nums1, target1);
 // console.assert(result1 === expected1, `Test case 1 failed: expected ${expected1} but got ${result1}`);
+
 // let nums2 = [1, 2, 3, 4, 5];
 // let target2 = 6;
 // let expected2 = -1;
 // let result2 = binarySearchFirstOccurrence(nums2, target2);
 // console.assert(result2 === expected2, `Test case 2 failed: expected ${expected2} but got ${result2}`);
+
 // let nums3 = [1, 1, 1, 2, 2, 2, 3, 3, 3];
 // let target3 = 2;
 // let expected3 = 3;
---
+++ day-3/problem3.js
@@ -0,0 +1,14 @@
+// Given a string, find the length of the longest substring without repeating characters.
+function lengthOfLongestSubstring(s) {
+    //turn the string into a letter array
+    let letterArray =[];
+    for(let i = 0; i< s.length; i++){
+        
+        letterArray.push(s[i])
+        //console.log(letterArray)
+    }
+}
+// console.log(lengthOfLongestSubstring("abcabcbb")); // Output: 3
+//[a, b, c, a, b,c, b, b]
+// console.log(lengthOfLongestSubstring("bbbbbb")); // Output: 1
+// console.log(lengthOfLongestSubstring("pwwkew")); // Output: 3
