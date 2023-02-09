26213ecbcd514085aeab67375dd252d64c7b3a76
Qibin <qbye0405@gmail.com>
2023-02-09 14:35:26 -0500

    passed p2 test case 4

--- day-3/problem2.js
+++ day-3/problem2.js
@@ -1,33 +1,30 @@
 // Given a sorted array of integers, find the first occurrence of a target value.
 // If the target is not found in the array, return -1.
 function binarySearchFirstOccurrence(nums, target) {
-    //edge case
+    
     if(nums.length <=0){
         return null
     }
-    //two index pointers: left and right
+   
     let left = 0; 
     let right = nums.length-1;
-    //while loop, find middle index
+   
     while(left<right){
         let middleIndex = Math.floor((left+right)/2)
         let middleValue = nums[middleIndex]
         if(middleValue == target){
-            if(middleValue == nums[middleIndex-1]){
-                return middleIndex-1
-            }else{
-                return middleIndex
+            for(let i = middleIndex-1; i>=0; i--){
+                if(nums[i] = target)
+                i--
             }
+            return middleIndex
         }else if(middleValue < target){
             left = middleIndex +1
         }else{
             right = middleIndex -1
         }
-
     }
-
     return -1
-
 }
 // let nums1 = [2, 2, 2, 2, 2];
 // Test case
@@ -48,3 +45,10 @@ function binarySearchFirstOccurrence(nums, target) {
 // let expected3 = 3;
 // let result3 = binarySearchFirstOccurrence(nums3, target3);
 // console.assert(result3 === expected3, `Test case 3 failed: expected ${expected3} but got ${result3}`);
+
+// let nums4 = [2, 2, 2, 2, 2];
+// let target4 = 2;
+// let expected4 = 0;
+// let result4 = binarySearchFirstOccurrence(nums4, target4);
+// console.log(result4)
+// console.assert(result4 === expected4, `Test case 4 failed: expected ${expected4} but got ${result4}`);
