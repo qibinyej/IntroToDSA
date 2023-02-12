65d149e588a102752fed1473be2bc49777dee3c6
Qibin <qbye0405@gmail.com>
2023-02-12 18:39:53 -0500

    p3 continuing

--- day-3/problem3.js
+++ day-3/problem3.js
@@ -1,10 +1,11 @@
 // Given a string, find the length of the longest substring without repeating characters.
+//sliding window: set up initial window, 
 function lengthOfLongestSubstring(s) {
     //turn the string into a letter array
     
 }
 // console.log(lengthOfLongestSubstring("abcabcbb")); // Output: 3
-//[a, b, c, a, b,c, b, b] 
+//[a, b, c, a, b,c, b, b]: initial window [a, b, c]; if 1st slide window === initial, pick 1st; ...
 
 // console.log(lengthOfLongestSubstring("bbbbbb")); // Output: 1
 // console.log(lengthOfLongestSubstring("pwwkew")); // Output: 3
