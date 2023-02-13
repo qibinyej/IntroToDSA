1c9932c080a17f3eb1a153c09efa1ba5a1b5e188
Qibin <qbye0405@gmail.com>
2023-02-13 16:50:13 -0500

    day-4{

--- day-4/problem1.js
+++ day-4/problem1.js
@@ -1,20 +1,22 @@
 // Write a recursive function called flatTheArray which accepts an array of arrays
 // and returns a new array with all values flattened.
 // flatTheArray([1, 2, 3, [4, 5] ]) // [1, 2, 3, 4, 5]
-// flatTheArray([1, [2, [3, 4], [[5]]]]) // [1, 2, 3, 4, 5]
+// flatTheArray([1, [2, [3, 4], [[5]]]]) // [1, 2, 3, 4, 5] // [1, 2, [3, 4], [[5]]] // arr.flat(2) - [1,2,3,4,[5]], arr.flat(3)
 // flatTheArray([[1],[2],[3]]) // [1,2,3]
 // flatTheArray([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]) // [1,2,3]
 
 const flatTheArray = (arr) => {
-  // recursive
-  // return a new array
-
   //recursion step 1: edge case
   if (arr.length == 0) {
     return -1;
   }
 
   //step 2: base case
-  let newArr = [];
-  
+ 
+  for(let i =0; i< arr.length; i++){
+    if(Array.isArray(arr[i])){
+        arr[i].flat
+    }
+  }
+
 };
