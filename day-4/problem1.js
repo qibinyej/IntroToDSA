98cfef86c48960a44cb8f02c588c4df9dd7f41e8
Qibin <qbye0405@gmail.com>
2023-02-13 00:03:51 -0500

    problem1

--- day-4/problem1.js
+++ day-4/problem1.js
@@ -6,5 +6,15 @@
 // flatTheArray([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]) // [1,2,3]
 
 const flatTheArray = (arr) => {
-    
-}
+  // recursive
+  // return a new array
+
+  //recursion step 1: edge case
+  if (arr.length == 0) {
+    return -1;
+  }
+
+  //step 2: base case
+  let newArr = [];
+  
+};
