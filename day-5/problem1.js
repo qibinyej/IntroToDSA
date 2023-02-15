b3efc6fd56e3deb23979426dbf3d073756a19379
Qibin <qbye0405@gmail.com>
2023-02-15 14:29:37 -0500

    shift

---
+++ day-5/problem1.js
@@ -0,0 +1,37 @@
+/**
+ Please construct the two classes with the appropriate properties for the Node and SLL, then for the SLL,
+ please write the methods for:
+ - Shift() remove first el and return removed el
+ - Unshift() add first el
+ - Push() add last el
+ - Pop() remove last el
+ - Get() acces el
+ - Set() create/update el
+ - Insert() insert el
+ - Remove() remove el
+ * 
+ */
+
+class Node {
+  constructor(val) {
+    this.data = val;
+    this.next = null;
+  }
+}
+class SinglyLinkedList {
+  constructor() {
+    this.head = head;
+    this.tail = tail;
+    this.length = 0;
+  }
+  
+  shift(){
+    if(this.length ==0){
+        return
+    }
+    if(this.head){
+        delete this.head
+    }
+    this.head
+  }
+}
