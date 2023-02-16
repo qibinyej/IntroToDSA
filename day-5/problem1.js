6fa0ea7700e471e2384afb5a5eb3f86465a381ca
Qibin <qbye0405@gmail.com>
2023-02-16 16:41:00 -0500

    pop

--- day-5/problem1.js
+++ day-5/problem1.js
@@ -18,20 +18,50 @@ class Node {
     this.next = null;
   }
 }
+
 class SinglyLinkedList {
   constructor() {
     this.head = head;
     this.tail = tail;
     this.length = 0;
   }
-  
-  shift(){
-    if(this.length ==0){
-        return
+
+  push(val){ //add new node to end of list, return new length;
+    let newNode = new Node(val);
+    if(!this.length){
+        this.head = newNode;
+    }else{ //add new node as a new tail node;
+        this.tail.next = newNode; //connect to the new node;
+        this.tail = newNode;  //set new node as tail node;
     }
-    if(this.head){
-        delete this.head
+    this.length++;
+
+  }
+
+  pop(){ //remove last node
+    let current = this.head; // create a temp node, start from left;
+    let newTail = current; // create a new tail node, start where current node is; positioning.
+    //use while loop to traverse the list; if the node next to current node hits last node, exit the loop.
+    while(current.next){
+        newTail = current; //but why??
+        current = current.next
     }
-    this.head
+    this.tail = newTail;
+    this.tail.next = null;
+  }
+
+  shift() {
+    if (this.length == 0) {
+      return;
+    }
+    if (this.head) {
+      this.head = null;
+    }
+    this.head;
   }
 }
+
+// test cases:
+// empty linked list
+// headnode
+//head-> node1-> node2-> tail
